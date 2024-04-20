b4fb836d90c507e3972172dfc261ee6838e7ceaf
ko gyeong tae <rudex1598@gmail.com>
2024-04-20 16:45:18 +0900

    누락 파일 재 추가

---
+++ monster_gewehr/Sever_Sysyem.h
@@ -0,0 +1,22 @@
+#pragma once
+
+struct PacketSend_Event {
+	CHAR id;
+	DirectX::XMFLOAT3 pos;
+	DirectX::XMFLOAT3 vel;
+	FLOAT yaw;
+	CHAR wepon;
+};
+class Sever_System : public EntitySystem,
+	public EventSubscriber<PacketSend_Event>
+{
+private:
+
+public:
+
+	Sever_System() = default;
+	virtual void configure(class World* world);
+	virtual void unconfigure(class World* world) {};
+	virtual void tick(class World* world, float deltaTime) {};
+	virtual void receive(class World* world, const PacketSend_Event& event);
+};
